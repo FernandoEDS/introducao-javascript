1358b990db4d2c06df6df44c2ada6316907d645e
Fernando Elizio <fernando.santos.viana@escola.pr.gov.br>
2025-11-17 09:58:54 -0300

    colocando funções

--- js/calcula-IMC.js
+++ js/calcula-IMC.js
@@ -43,35 +43,26 @@ for (var i=0; i < pacientes.length; i++) {
 
 }
 
-var botaoAdicionar = document.querySelector("#adicionar-paciente");
+// reescrever o mesmo código em três funções independentes 
 
-botaoAdicionar.addEventListener("click", function(){
-    event.preventDefault();
-    var form = document.querySelector("#form-adiciona");
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
+function calculaIMC(){
+    var IMC = 0;
+    IMC = peso/(altura * altura);
+    return IMC.toFixed(2)
+}
 
-    var pacienteTr = document.createElement("tr");
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-    var imcTd = document.createElement("td");
+function validaPeso(peso){
+    if (peso <= 0 || peso >= 1000) {
+        return false;
+    } else {
+        return true;
+    }
+}
 
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-
-    var tabela = document.querySelector("#tabela-pacientes");
-
-    tabela.appendChild(pacienteTr);
-
-});
+function validaAltura(altura) {
+    if (altura <=0 || altura > 3.00){
+         return false;
+    } else {
+        return true;
+    }
+}
