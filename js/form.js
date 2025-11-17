ebd74d5eb1c2254290e550a9dd7375621af14d72
Fernando Elizio <fernando.santos.viana@escola.pr.gov.br>
2025-11-17 11:02:08 -0300

    criando duas de três funções

--- js/form.js
+++ js/form.js
@@ -6,25 +6,7 @@ botaoAdicionar.addEventListener("click", function(){
     //Extrai informações do Paciente do form
     obtemPacienteDoFormulario(form);
     
-    var pacienteTr = document.createElement("tr");
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-    var imcTd = document.createElement("td");
-
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-    imcTd.testcontent = calculaIMC(peso,altura);
-
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-    pacienteTr.appendChild(IMCTd);
-
+    
     var tabela = document.querySelector("#tabela-pacientes");
 
     tabela.appendChild(pacienteTr);
@@ -34,12 +16,38 @@ botaoAdicionar.addEventListener("click", function(){
 // dividir em três funções para trabalhar com tarefas mwnores
 function obtemPacienteDoFormulario(form){
     var paciente = {
-        nome:
-        peso:
-        altura:
-        gordura:
+        nome:form.nome.value,
+        peso:form.peso.value,
+        altura:form.altura.value,
+        gordura:form.gordura.value,
+        IMC: calculaIMC(form.peso.value, form.altura.value);
     }
 
     return paciente;
    
+}
+//cria a tr e a td do paciente
+var paciente = montair(paciente);
+
+function montair(paciente){
+   
+    var pacienteTr = document.createElement("tr");
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = paciente.nome;
+    pesoTd.textContent = paciente.peso;
+    alturaTd.textContent = paciente.altura;
+    gorduraTd.textContent = paciente.gordura;
+    imcTd.testcontent = paciente.IMC;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(IMCTd);
+
 }
